8a02e91bc9ecbbc1a7d11d4dd8219a38e2bddc97
gigabit101 <thegigabit101@gmail.com>
2016-05-21 01:56:07 +0100

    Fixed formatting

--- src/main/java/NEILootbags/nei/NEILootbagHandler.java
+++ src/main/java/NEILootbags/nei/NEILootbagHandler.java
@@ -19,16 +19,16 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
-public class NEILootbagHandler extends TemplateRecipeHandler
-{ 
-    private static final int X_FIRST_ITEM = 2;
-    private static final int Y_FIRST_ITEM = 51;
-    
-    private static final int ITEMS_PER_PAGE = 9 * 4;
-    private static final int SPACING_X = 166 / 9;
-    private static final int SPACING_Y = 80 / 4;
-    private static int bagtype;
-    
+public class NEILootbagHandler extends TemplateRecipeHandler 
+{
+	private static final int X_FIRST_ITEM = 2;
+	private static final int Y_FIRST_ITEM = 51;
+
+	private static final int ITEMS_PER_PAGE = 9 * 4;
+	private static final int SPACING_X = 166 / 9;
+	private static final int SPACING_Y = 80 / 4;
+	private static int bagtype;
+
 	@Override
 	public String getRecipeName() 
 	{
@@ -40,32 +40,32 @@ public class NEILootbagHandler extends TemplateRecipeHandler
 	{
 		return "neilootbags:textures/gui/NEILootGUI1.png";
 	}
-	
-    @Override
-    public void drawBackground(int recipe)
-    {
-        GL11.glColor4f(1, 1, 1, 1);
-        GuiDraw.changeTexture(getGuiTexture());
-        GuiDraw.drawTexturedModalRect(0, 0, 5, 11, 166, 130);
-    }
-	
-    @Override
-    public int recipiesPerPage()
-    {
-        return 1;
-    }
 
 	@Override
-    public void loadCraftingRecipes(ItemStack result)
-    {
-    	bagtype = 0;
-		for (int k = 0; k < 4; k++)
+	public void drawBackground(int recipe) 
+	{
+		GL11.glColor4f(1, 1, 1, 1);
+		GuiDraw.changeTexture(getGuiTexture());
+		GuiDraw.drawTexturedModalRect(0, 0, 5, 11, 166, 130);
+	}
+
+	@Override
+	public int recipiesPerPage() 
+	{
+		return 1;
+	}
+
+	@Override
+	public void loadCraftingRecipes(ItemStack result) 
+	{
+		bagtype = 0;
+		for (int k = 0; k < 4; k++) 
 		{
 			Bag b = BagHandler.getBag(k);
 
-			for (int i = 0; i < b.getMap().size(); i++)
+			for (int i = 0; i < b.getMap().size(); i++) 
 			{
-				if(LootBags.areItemStacksEqualItem(b.getSpecificItem(i), result, false, false))
+				if (LootBags.areItemStacksEqualItem(b.getSpecificItem(i), result, false, false)) 
 				{
 					arecipes.add(new CachedLootRecipe(result, k));
 					this.bagtype = k;
@@ -73,66 +73,68 @@ public class NEILootbagHandler extends TemplateRecipeHandler
 				}
 			}
 		}
-    }
-    
-    @Override
-    public void drawExtras(int recipe)
-    {
-    	CachedLootRecipe cachedloot = (CachedLootRecipe) arecipes.get(recipe);
-    }
-    
-	public class CachedLootRecipe extends TemplateRecipeHandler.CachedRecipe
+	}
+
+	@Override
+	public void drawExtras(int recipe) 
+	{
+		CachedLootRecipe cachedloot = (CachedLootRecipe) arecipes.get(recipe);
+	}
+
+	public class CachedLootRecipe extends TemplateRecipeHandler.CachedRecipe 
 	{
 		private ItemStack stack;
-        public int set, lastSet;
-        private long cycleAt;
+		public int set, lastSet;
+		private long cycleAt;
 		public List<ItemStack> outputs;
-		
-		public CachedLootRecipe(ItemStack stack, int bagtype)
+
+		public CachedLootRecipe(ItemStack stack, int bagtype) 
 		{
 			this.stack = stack;
-            set = 0;
+			set = 0;
 			this.outputs = new ArrayList<ItemStack>();
-            lastSet = (this.outputs.size() / (20 + 1));
-    		Bag b = BagHandler.getBag(bagtype);
+			lastSet = (this.outputs.size() / (20 + 1));
+			Bag b = BagHandler.getBag(bagtype);
 
-    		for (int i = 0; i < b.getMap().size(); i++)
-    		{
-    			outputs.add(b.getSpecificItem(i));    			
-    		}
+			for (int i = 0; i < b.getMap().size(); i++) 
+			{
+				outputs.add(b.getSpecificItem(i));
+			}
 		}
-		
+
 		@Override
 		public PositionedStack getIngredient() 
 		{
-			return new PositionedStack(new ItemStack(LootBags.lootbagItem, 1, bagtype), 75, 5);		
+			return new PositionedStack(new ItemStack(LootBags.lootbagItem, 1, bagtype), 75, 5);
 		}
-		
+
 		@Override
 		public PositionedStack getResult() 
 		{
 			return new PositionedStack(this.stack, X_FIRST_ITEM, Y_FIRST_ITEM);
-		}		
-		
-        @Override
-        public List<PositionedStack> getOtherStacks()
-        {
-            List<PositionedStack> list = new ArrayList<PositionedStack>();
-            int x = X_FIRST_ITEM;
-            int y = Y_FIRST_ITEM;
-            for (int i = ITEMS_PER_PAGE  * set; i < ITEMS_PER_PAGE  * set + ITEMS_PER_PAGE ; i++)
-            {
-                if (i >= this.outputs.size()) break;
-                list.add(new PositionedStack(this.outputs.get(i), x, y));
-                y += SPACING_Y;
-                if (y >= Y_FIRST_ITEM + SPACING_Y * 4)
-                {
-                    y = Y_FIRST_ITEM;
-                    x += SPACING_X;
-                }
-            }
-            if (list.size() > 0) list.remove(0);
-            return list;
-        }
+		}
+
+		@Override
+		public List<PositionedStack> getOtherStacks() 
+		{
+			List<PositionedStack> list = new ArrayList<PositionedStack>();
+			int x = X_FIRST_ITEM;
+			int y = Y_FIRST_ITEM;
+			for (int i = ITEMS_PER_PAGE * set; i < ITEMS_PER_PAGE * set + ITEMS_PER_PAGE; i++) 
+			{
+				if (i >= this.outputs.size())
+					break;
+				list.add(new PositionedStack(this.outputs.get(i), x, y));
+				y += SPACING_Y;
+				if (y >= Y_FIRST_ITEM + SPACING_Y * 4) 
+				{
+					y = Y_FIRST_ITEM;
+					x += SPACING_X;
+				}
+			}
+			if (list.size() > 0)
+				list.remove(0);
+			return list;
+		}
 	}
 }
